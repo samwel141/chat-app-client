b008998440ede30067644bddd8176fb646ce7201
samwel141 <samwelwilson141@gmail.com>
2024-03-10 09:51:43 +0300

    Initial commit

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+/* eslint-disable eqeqeq */
+import { useEffect, useState } from 'react'
+
+const PREFIX = 'whatsapp-clone-'
+
+export default function useLocalStorage(key, initialValue) {
+  const prefixedKey = PREFIX + key
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey)
+    if (jsonValue != null) return JSON.parse(jsonValue)
+    console.log(jsonValue)
+    if (typeof initialValue === 'function') {
+      return initialValue()
+    } else {
+      return initialValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value))
+  }, [prefixedKey, value])
+
+  return [value, setValue]
+}
